82f93f5acac7d1dfa0510a4b649abd321bd0270f
Steffen Schulz <steffen.schulz@intel.com>
2020-09-28 17:59:28 +0200

    bump to linux-5.8.12

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 #LINUX_VERSION="5.4.55"
 LINUX_VERSION="5.7.12"
-LINUX_VERSION="5.8.11"
+LINUX_VERSION="5.8.12"
 LINUX_URL="https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${LINUX_VERSION}.tar.xz"
 
 #QEMU_VERSION="4.2.0"
