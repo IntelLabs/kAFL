cc4d0fa053bc1f64b8eef732dea6940f93492338
Steffen Schulz <steffen.schulz@intel.com>
2021-12-17 01:50:51 +0100

    mcat.py: fix typo, turn into standalone tool

--- tools/mcat.py
+++ tools/mcat.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 #
 # Copyright 2019 Sergej Schumilo, Cornelius Aschermann, Tim Blazytko
-# Copyright 2020 Intel Corporation
+# Copyright 2021 Intel Corporation
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 """
-Pretty-Pring msgpack files produced by kAFL
+Pretty-Print msgpack files produced by kAFL
 """
 
 import os
@@ -15,8 +15,9 @@ import sys
 import msgpack
 from pprint import pprint
 
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../kAFL-Fuzzer/")
-from common.util import read_binary_file
+def read_binary_file(filename):
+    with open(filename, 'rb') as f:
+        return f.read()
 
 for arg in sys.argv[1:]:
     pprint(msgpack.unpackb(read_binary_file(arg), raw=False, strict_map_key=False))
