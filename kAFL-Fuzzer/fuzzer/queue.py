4ad3ff8d982fe5bd48c779e305edd453b010019c
Steffen Schulz <steffen.schulz@intel.com>
2021-12-22 18:53:39 -0800

    scheduler: rebalance perf/impact scoring
    - simpler short queue selection, deprecate should_be_scheduled
    - adjust log factors to balance payload size/favs vs fuzzing stage and time
    - recompute impact only on perf or fav bit update

--- kAFL-Fuzzer/fuzzer/queue.py
+++ kAFL-Fuzzer/fuzzer/queue.py
@@ -25,11 +25,10 @@ class InputQueue:
 
         while self.current_cycle:
             node = self.current_cycle.pop()
-            if self.scheduler.should_be_scheduled(self, node):
-                if not node.is_busy():
-                    if node.get_state() != "final":
-                        node.set_busy()
-                    return node
+            if not node.is_busy():
+                if node.get_state() != "final":
+                    node.set_busy()
+                return node
 
         self.update_current_cycle()
 
@@ -46,16 +45,22 @@ class InputQueue:
         # for slow targets since we don't have good queue culling and early
         # Redqueen/Grimoire stages seem to be the most efficient.
         #
-        # TODO: Sorting the queue is relatively expensive and can turn the
-        # master into a bottleneck. Experiment with cylce_factor to find a nice
-        # compromise, or fix Slaves to return less often.
-        cycle_factor = 2
-        cycle_size = int(cycle_factor*self.num_slaves)
+        # Issues
+        # - Sorting the queue is relatively expensive, can turn the master into a bottleneck
+        # - If we have very few items, we will end up sorting all the time
+        #
+        # Alternatives?
+        # - keep a sorted queue
+        # - let scheduler pick randomly, with weighted distribution
+
+        fav_items = self.statistics.data['favs_total']
+        cycle_size = int(min(1.5*fav_items, 4*self.num_slaves))
+
+        full_queue = sorted(self.id_to_node.values(),
+                            key=lambda n: self.scheduler.score_priority_favs(n))
 
         self.num_cycles += 1
-        self.current_cycle = list(self.id_to_node.values())
-        self.sort_queue(self.current_cycle)
-        self.current_cycle = self.current_cycle[-cycle_size:]
+        self.current_cycle = full_queue[-cycle_size:]
         self.statistics.event_queue_cycle(self)
 
         #for i in self.current_cycle:
@@ -72,15 +77,6 @@ class InputQueue:
         #        i.get_state(),
         #        ))
 
-    def sort_queue(self, nodes):
-        nodes.sort(key=lambda n: self.scheduler.score_priority_favs(n))
-
-    def get_node_by_id(self, nid):
-        return self.id_to_node[nid]
-
-    def num_inputs(self):
-        return len(self.id_to_node)
-
     def maybe_pushback_to_cycle(self, node):
         # put nodes in early stages directly at head of queue, to reduce global sorting
         if node.get_exit_reason() == "regular" and node.get_state() in ["initial"]:
@@ -88,21 +84,29 @@ class InputQueue:
                 self.current_cycle.append(node)
 
     def update_node_results(self, nid, results, new_payload):
-        node = self.get_node_by_id(nid)
+        node = self.id_to_node[nid]
         self.statistics.event_node_update(node, results)
-        node.update_metadata(results)
         if new_payload:
             node.set_payload(new_payload)
             node.node_struct["info"]["trimmed"] = True
+            node.set_score(self.scheduler.score_speed(node))
+        if results.get("performance"):
+            oldperf = node.get_initial_performance()
+            newperf = results["performance"]
+            #print("perf updated for node %d: %.2f => %.2f" % (node.get_id(), oldperf*1000,newperf*1000))
+            node.set_score(self.scheduler.score_speed(node))
+
+        node.set_fav_factor(self.scheduler.score_impact(node), write=False)
+        node.update_metadata(results)
         node.set_free()
         self.maybe_pushback_to_cycle(node)
 
     def insert_input(self, node, bitmap):
         parent = node.get_parent_id()
-        node.set_level(self.get_node_by_id(parent).get_level() + 1 if parent else 0, write=False)
+        node.set_level(self.id_to_node[parent].get_level() + 1 if parent else 0, write=False)
         node.set_performance(node.get_initial_performance(), write=False)
         node.clear_fav_bits(write=False)
-        node.set_fav_factor(self.scheduler.score_speed(node), write=True)
+        node.set_score(self.scheduler.score_speed(node))
 
         self.id_to_node[node.get_id()] = node
 
@@ -112,6 +116,8 @@ class InputQueue:
                 self.update_best_input_for_bitmap_entry(node, bitmap)  # TODO improve performance!
                 self.maybe_pushback_to_cycle(node)
 
+        node.set_fav_factor(self.scheduler.score_impact(node), write=True)
+        #node.update_file()
         self.statistics.event_node_new(node)
 
     def should_overwrite_old_entry(self, index, val, node):
@@ -119,9 +125,9 @@ class InputQueue:
         if not entry:
             return True, None
         old_node, old_val = entry
-        #more_bits = val > old_val
-        better_score = node.get_fav_factor() <= old_node.get_fav_factor()
-        if better_score:
+        better_bits = val > old_val and node.get_score() <= old_node.get_score()
+        better_score = val == old_val and node.get_score() < old_node.get_score()
+        if better_bits or better_score:
             return True, old_node
         return False, None
 
--- kAFL-Fuzzer/fuzzer/scheduler.py
+++ kAFL-Fuzzer/fuzzer/scheduler.py
@@ -12,93 +12,79 @@ high-scoring fav nodes will also go through deterministic stages. Particularly
 strong fav nodes may overcome the stage buff and go all the way to havoc before
 others are done.
 
+Additionally, the time that a node has already spent in fuzzing is used to
+dynamically reduce its score. This means new found nodes will be prioritized
+until they reached similar attention time in relation to their basic
+performance/impact score. Also slow nodes that easily lead to timeouts/crashes
+will be de-emphasized faster as their attention time compensates for the early
+stage buff.
+
 Queue sorting can become a bottleneck on large queues or very fast
 execution/finding rate.
-
 """
 
 
-from fuzzer.technique.helper import rand
-from math import log, log2, ceil
+from math import log, log2, log10, ceil
+from common.log import logger
 
 # scale arbitrarily large / small inputs down to interval [1,scale]
 # supply alternative log to get a better fit
-def log_scale(value, scale=1, base=2):
-
-    if value <= base:
-        return 1
+def log_scale(value, base=2):
 
     if base == 2:
-        val = log2(value)
+        return log2(base+value)
+    elif base == 10:
+        return log10(base+value)
     else:
-        val = log(value, base)
-
-    return ceil(scale*val-scale+1)
+        return log(base+value, base)
 
 
 class Scheduler:
 
-    def __init__(self):
-        pass
-
-    # TODO: node skipping by p(x) conflicts with queue sorting..
-    def should_be_scheduled(self, queue, node):
-        SKIP_CRASHING_PROB = 95
-        SKIP_NONFAV_PROB = 70
-
-        if node.get_exit_reason() != "regular":
-            if rand.int(100) < SKIP_CRASHING_PROB:
-                return False
-
-        if not node.is_favorite() and rand.int(100) < SKIP_NONFAV_PROB:
-            return False
-        return True
-
     def score_impact(self, node):
-        # each fav bit counts 8 times the depth level
-        impact = 8*len(node.get_fav_bits()) + node.get_level()
-        return log_scale(impact, scale=5)
+        # compute payload priority based on fav bits and perf score
+        return 10*log_scale(len(node.get_fav_bits())) / log_scale(node.get_score())
 
     def score_speed(self, node):
-        p_len = node.get_payload_len()
-        p_len = 1 if p_len == 0 else p_len
-        return log_scale(10000/(node.get_performance()*p_len), scale=6, base=256)
+        # payload runtime * length, lower is better
+        # apply log scale such that notable changes in KB or msec lead to different score
+        # resulting score granuarily should work well as bucket size for favs filtering
+        min_time=1/1000
+        p_time = log_scale(node.get_performance() / min_time)
+        p_len  = log_scale(node.get_payload_len() / 1024)
+        return ceil(p_time*p_len)
 
     def score_priority_favs(self, node):
-        score = node.get_fav_factor() # score_speed()
+        # assign scheduler priority based on prio, and special stage/type buffs
+        # consider total time already spend on this node to promote later discovered nodes
+        prio = node.get_fav_factor()
+        time_spent = node.node_struct.get("attention_secs",0) / 60
+        time_buff = log_scale(time_spent)
 
-        # below stage buffs are invalid for busy nodes.
-        # sort in with <final> nodes as these are the only ones we can process in parallel
-        if node.is_busy() or node.get_exit_reason() != "regular":
-            return (1, score)
-
-        # boost nodes deeper in the tree
-        #if node.get_level() > 0:
-        #    score += node.get_level()//5
-
-        # boost nodes with many fav bits
-        if node.is_favorite():
-            score *= 2*len(node.get_fav_bits())
-
-        # TODO: only actually have to compute all this for new nodes and fav bit changes...
-        node.set_score(score)
-
-        if node.get_state() in ["initial", "redq/grim"]:
-            phase = 64
+        # assign special buff based on node type or stage
+        if node.get_exit_reason() != "regular":
+            phase = 1/10
+        elif node.is_busy():
+            # avoid filling priority list with busy nodes
+            phase = 1
+        elif node.get_state() in ["initial"]:
+            phase = 16
+        elif node.get_state() in ["redq/grim"]:
+            phase = 8
         elif node.get_state() in ["deterministic"]:
             phase = 4
         elif node.get_state() in ["havoc"]:
             phase = 2
         elif node.get_state() in ["final"]:
-            # promote later discovered nodes by compensating for total time spend in havoc.
-            # TODO some nodes are buffed on purpose - should only promote based on relative
-            # time or cycles rather than total face time
-            time_spent = node.node_struct.get("state_time_havoc",1)
-            score = score/log_scale(time_spent)
-            return (1, score)
+            phase = 1
         else:
             assert(False), "unknown state"
 
-        # first solve all initial phases, and highest-ranking score/impact there
-        return (score*phase, score)
+        if not node.is_favorite():
+            phase /= 5
+
+        #logger.info(
+        #        "%s: node %3d rated %5.2f [phase=%3d, prio=%.2f, tbuff=%.2f]" % (
+        #            "Sched", node.get_id(), prio*phase/time_buff, phase, prio, time_buff))
+        return prio*phase/time_buff
 
